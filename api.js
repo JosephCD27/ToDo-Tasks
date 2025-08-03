dcc5081f2b2142c4a82a8f03782eff16c542f167
JosephCD27 <joseph.camacho2005@gmail.com>
2025-08-03 10:07:26 -0500

    creación de rutas

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var express = require("express");
 var router = express.Router();
-
+var TaskModel = require('./task_schema');
 
 let environment = null;
 
@@ -36,3 +36,67 @@ mongoose.connect(db, {
 });
 
 module.exports = router;
+
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+
+    var newTask = new TaskModel(task);
+
+    newTask.save()
+        .then(data => {
+            res.status(200).send("OK\n");
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        });
+});
+
+
+router.get('/all-tasks', function (req, res) {
+    TaskModel.find()
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        });
+});
+
+
+router.post('/update-task', function (req, res) {
+    TaskModel.updateOne(
+        { TaskId: req.body.TaskId }, 
+        {
+            Name: req.body.Name,
+            Deadline: req.body.Deadline
+        }
+    )
+        .then(data => {
+            res.status(200).send("OK\n");
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        });
+});
+
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId })
+        .then(data => {
+            res.status(200).send("OK\n");
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        });
+});
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const api = require('./api');
 const app = express()
 const port = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`)
 })
